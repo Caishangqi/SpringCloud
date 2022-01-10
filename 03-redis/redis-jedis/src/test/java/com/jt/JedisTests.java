5f93c5c8d43659be60f77e2788f7f5b59e7f5c3e
Caishangqi <2470159730@qq.com>
2022-01-10 01:36:16 -0800

    Ping Redis

--- 03-redis/redis-jedis/src/test/java/com/jt/JedisTests.java
+++ 03-redis/redis-jedis/src/test/java/com/jt/JedisTests.java
@@ -7,8 +7,12 @@ public class JedisTests {
 
     @Test
     public void testGetConnection(){
-        Jedis jedis = new Jedis("192.168.126.129",6379);
-        String result = jedis.ping();
-        System.out.println(result);
+        //假如不能连通,要注释掉redis.conf中 bind 127.0.0.1,
+        //并将protected-mode的值修改为no,然后重启redis再试
+        Jedis jedis=new Jedis("192.168.126.129",6379);
+        //jedis.auth("123456");//假如在redis.conf中设置了密码
+        String ping = jedis.ping();
+        System.out.println(ping);
     }
+
 }
